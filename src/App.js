b1a6581975fcf664f3f47fdc3b9a307ce92bb0a2
John <john.milner@mac.com>
2018-02-10 19:22:17 -0800

    update Register, Signin and App.js

--- src/App.js
+++ src/App.js
@@ -34,9 +34,32 @@ class App extends Component {
       imageUrl: '',
       box: [],
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
   }
+
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
+  }
+
+  // componentDidMount() {
+  //   fetch('http://localhost:3000/')
+  //   .then(response => response.json())
+  //   .then(console.log)
+  // }
 
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions;
@@ -109,7 +132,7 @@ class App extends Component {
           : (
           route === 'signin' 
           ? <Signin onRouteChange={this.onRouteChange}/>
-          : <Register onRouteChange={this.onRouteChange}/>
+          : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
           )
           }
         </div>
