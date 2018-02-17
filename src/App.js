a61d6e1a4ff04b0c6ff60b1d133a17c50c6cde00
John Milner <john.milner@mac.com>
2018-02-17 00:50:39 -0800

    deploy

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://powerful-retreat-11238.herokuapp.com/imageurl', {
       method: 'post',
       headers: {
         'Content-Type': 'application/json',
@@ -100,7 +100,7 @@ class App extends Component {
     .then(response => response.json())
     .then(response => {
       if (response) {
-        fetch('http://localhost:3000/image', {
+        fetch('https://powerful-retreat-11238.herokuapp.com/image', {
           method: 'put',
           headers: {
             'Content-Type': 'application/json',
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -16,7 +16,7 @@ class Signin extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://powerful-retreat-11238.herokuapp.com/signin', {
             method: 'post',
             headers: {
                 'Content-Type': 'application/json',
